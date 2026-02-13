dfac064fbb80a266179dbf997575e63a0b21117e
Filip Petrovski <filip.petrovski1992@gmail.com>
2026-02-13 18:37:51 +0100

    feat: add image carousels on product

--- src/features/products/product/product.component.ts
+++ src/features/products/product/product.component.ts
@@ -1,17 +1,34 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { Product } from '../models/product.class';
 import { CommonModule } from '@angular/common';
+import { ImageCarouselComponent } from '../../../shared/components/image-carousel/image-carousel/image-carousel.component';
 
 @Component({
-  selector: 'pg-product',
-  imports: [CommonModule],
-  templateUrl: './product.component.html',
+    selector: 'pg-product',
+    imports: [CommonModule, ImageCarouselComponent],
+    templateUrl: './product.component.html',
 })
 export class ProductComponent {
-  product = input.required<Product>();
-  addToCart = output<Product>();
+    product = input.required<Product>();
+    currentImgIndex = signal(0);
 
-  onAddToCartClick() {
-    this.addToCart.emit(this.product());
-  }
+    addToCart = output<Product>();
+
+    onAddToCartClick() {
+        this.addToCart.emit(this.product());
+    }
+
+    nextImage(event: Event) {
+        event.stopPropagation();
+        const next = (this.currentImgIndex() + 1) % this.product().images.length;
+        this.currentImgIndex.set(next);
+    }
+
+    prevImage(event: Event) {
+        event.stopPropagation();
+        const prev =
+            (this.currentImgIndex() - 1 + this.product().images.length) %
+            this.product().images.length;
+        this.currentImgIndex.set(prev);
+    }
 }
