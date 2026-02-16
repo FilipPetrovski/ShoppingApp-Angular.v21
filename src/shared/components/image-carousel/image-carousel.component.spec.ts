bd71126f161ad4ce37d1aed6876f1e21e56eaf44
FilipPetrovski <53006578+FilipPetrovski@users.noreply.github.com>
2026-02-16 21:05:37 +0100

    feat: configure testing environment and add browser for running tests (#5)

--- src/shared/components/image-carousel/image-carousel.component.spec.ts
+++ src/shared/components/image-carousel/image-carousel.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ImageCarouselComponent } from './image-carousel/image-carousel.component';
+import { ImageCarouselComponent } from './image-carousel.component';
 
 describe('ImageCarouselComponent', () => {
   let component: ImageCarouselComponent;
